baeb126f7f8d5ec1b81898f767583be4bfb73f23
Paulo Júnior <pjunior954@gmail.com>
2026-09-03 14:36:57 -0300

    fix: Reduce information of My Journey

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Estratégia: cache-first para a casca do app (funciona 100% offline),
  * network-first para o HTML (para pegar atualizações quando houver rede).
  */
-const VERSION = 'florescer-v1.22.0';
+const VERSION = 'florescer-v1.30.0';
 const SHELL = [
   './',
   './index.html',
